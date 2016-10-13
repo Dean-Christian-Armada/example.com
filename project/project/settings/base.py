9efbe80e7f0a0b7d7dfc6966059488862dbab577
Dean Armada <deanarmada@gmail.com>
2016-10-13 10:22:21 +0800

    Integrated Admin Template and Static Files

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -14,7 +14,7 @@ import os
 import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SETTINGS_DIR = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
