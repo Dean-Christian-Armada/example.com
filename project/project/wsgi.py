e5422da9516aa3a2567832918610dc4c7242224c
Dean Armada <deanarmada@gmail.com>
2016-10-16 22:07:14 +0800

    Used the virtualenv earlier before django import

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+VENV_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+activate_env=os.path.expanduser(os.path.join(VENV_DIR, 'bin/activate_this.py'))
+execfile(activate_env, dict(__file__=activate_env))
+
 from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings"
 
-VENV_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-activate_env=os.path.expanduser(os.path.join(VENV_DIR, 'bin/activate_this.py'))
-execfile(activate_env, dict(__file__=activate_env))
 
 application = get_wsgi_application()
