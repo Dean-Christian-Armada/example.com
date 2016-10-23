a09e1c4a1bbe106f04a4c11625461ba282c330f5
Dean Armada <deanarmada@gmail.com>
2016-10-23 15:36:47 +0800

    Model Manager Improvement

--- project/core/model_managers.py
+++ project/core/model_managers.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 
 # http://stackoverflow.com/questions/1512059/django-get-an-object-form-the-db-or-none-if-nothing-matches 
-class GetOrNoneManager(models.Manager):
+class GetManager(models.Manager):
     #Adds get_or_none method to objects
     def get_or_none(self, **kwargs):
         try:
